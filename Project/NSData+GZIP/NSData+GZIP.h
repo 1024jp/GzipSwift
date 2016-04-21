830e50c35d1fbe326167640eadaa5e0449876e9c
Matthew Purland <m.purland@gmail.com>
2016-04-21 13:58:32 -0700

    Added framework target and scheme NSData_GZIP for Carthage support

---
+++ Project/NSData+GZIP/NSData+GZIP.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSData+GZIP.
+FOUNDATION_EXPORT double NSData_GZIPVersionNumber;
+
+//! Project version string for NSData+GZIP.
+FOUNDATION_EXPORT const unsigned char NSData_GZIPVersionString[];
