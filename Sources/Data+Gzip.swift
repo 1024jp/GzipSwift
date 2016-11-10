d3a34f0a7114ee7137a368053ee8d8f94dadbf3f
1024jp <1024jp@wolfrosch.com>
2016-11-10 18:22:27 +0900

    Tweak error message for Linux/FreeBSD
    Because it acutally doesn't localized.

--- Sources/Data+Gzip.swift
+++ Sources/Data+Gzip.swift
@@ -150,12 +150,8 @@ public enum GzipError: Error {
                 return message
             }
         }()
-
-        #if os(Linux) || os(FreeBSD) 
-        return "Error: \(description)"
-        #else       
-        return NSLocalizedString(description, comment: "error message")
-        #endif
+        
+        return description
     }
     
 }
