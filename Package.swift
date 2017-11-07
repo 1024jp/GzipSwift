dcdf20640d48b2c58a5b9a442696ca7412d8220d
1024jp <1024jp@wolfrosch.com>
2017-11-08 08:58:30 +0900

    Adjust whitespaces and blank lines in code

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 let package = Package(
     name: "Gzip",
     products: [
-      .library(name: "Gzip", targets: ["Gzip"])
+        .library(name: "Gzip", targets: ["Gzip"]),
     ],
     targets: [
-      .target(name: "Gzip", dependencies: ["system-zlib"]),
-      .target(name: "system-zlib"),
-      .testTarget(name: "GzipTests", dependencies: ["Gzip"])
+        .target(name: "Gzip", dependencies: ["system-zlib"]),
+        .target(name: "system-zlib"),
+        .testTarget(name: "GzipTests", dependencies: ["Gzip"]),
     ]
 )
