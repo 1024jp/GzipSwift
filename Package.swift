b202b1101f32743d583b3925d56af8bc960d9dac
Daniel <daniel@danramteke.com>
2017-11-07 11:47:13 -0500

    trying to get to work under linux

--- Package.swift
+++ Package.swift
@@ -1,6 +1,16 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "Gzip",
-    dependencies: [.Package(url: "https://github.com/1024jp/zlib.git", versions: Version(0, 0, 0)..<Version(1, 0, 0))]
+    products: [
+      .library(name: "Gzip", targets: ["Gzip"])
+    ],
+    dependencies: [],
+    targets: [
+      .target(name: "Gzip", dependencies: ["zlib"]),
+      .target(name: "zlib"),
+      .testTarget(name: "GzipTests", dependencies: ["Gzip"])
+    ]
 )
